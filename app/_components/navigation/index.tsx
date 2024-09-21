e1044f5e87b83f04afa9c66896526cb71b8ac08e
Minsup Lee <m1nsuppp@gmail.com>
2024-09-21 19:54:00 +0900

    next/navigation(app router) 테스트 (#2)
    * [chore] prettier 설정
    
    * [feat] next/navigation 모킹
    
    * [test] next/navigation 테스트

---
+++ app/_components/navigation/index.tsx
@@ -0,0 +1,47 @@
+'use client';
+
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+type NavigationItem = {
+  id: string;
+  label: string;
+  href: string;
+};
+
+const navigationItems: NavigationItem[] = [
+  { id: 'home', label: 'Home', href: '/' },
+  { id: 'about', label: 'About', href: '/about' },
+  { id: 'contact', label: 'Contact', href: '/contact' },
+];
+
+export function Navigation(): JSX.Element {
+  const pathname = usePathname();
+
+  const isCurrentPathname = (href: string) => {
+    return pathname === href;
+  };
+
+  return (
+    <nav
+      aria-label="navigation"
+      className="p-4 w-fit h-screen bg-gray-800 text-white"
+    >
+      <ul className="flex flex-col items-start justify-center gap-2">
+        {navigationItems.map((item) => {
+          return (
+            <li key={item.id}>
+              <Link
+                className="aria-[current=page]:text-red-400"
+                aria-current={isCurrentPathname(item.href) ? 'page' : undefined}
+                href={item.href}
+              >
+                {item.label}
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
+}
--- jest.setup.ts
+++ jest.setup.ts
@@ -1,6 +1,1 @@
 import '@testing-library/jest-dom';
-
-jest.mock('next/navigation', () => ({
-  ...require('next-router-mock'),
-  useSearchParams: () => [[{ revalidate: '1' }]],
-}));
