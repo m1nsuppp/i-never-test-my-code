2cc914d2963dae82a58d0a897b2e7ccc824c262a
Minsup Lee <m1nsuppp@gmail.com>
2024-11-30 15:19:05 +0900

    [feat] integrate Suspense for article loading and switch to useSuspenseQuery

--- app/(pages)/(home)/_components/article-list.tsx
+++ app/(pages)/(home)/_components/article-list.tsx
@@ -4,15 +4,15 @@ import { useQuery } from '@/app/_shared/hooks/use-query';
 import { useTypedContext } from '@/app/_shared/hooks/use-typed-context';
 import { ServiceContext } from '@/app/_shared/providers/service-provider';
 import { Article } from '@/entities/article';
+import { useSuspenseQuery } from '@tanstack/react-query';
 
 export function ArticleList(): JSX.Element {
   const { articlesService } = useTypedContext(ServiceContext);
 
-  const { data: articles } = useQuery<Article[]>({
+  const { data: articles } = useSuspenseQuery<Article[], Error>({
+    queryKey: ['articles'],
     queryFn: articlesService.fetchArticles,
   });
-
-  if (!articles) return <div>loading...</div>;
 
   return (
     <ul>
--- app/(pages)/(home)/page.tsx
+++ app/(pages)/(home)/page.tsx
@@ -1,5 +1,23 @@
+import { Suspense } from 'react';
 import { ArticleList } from './_components/article-list';
 
 export default function ArticlesPage(): JSX.Element {
-  return <ArticleList />;
+  return (
+    <Suspense
+      fallback={
+        <div
+          role="status"
+          className="flex flex-wrap gap-3"
+        >
+          <div className="w-20 h-20 bg-gray-200 animate-pulse" />
+          <div className="w-20 h-20 bg-gray-200 animate-pulse" />
+          <div className="w-20 h-20 bg-gray-200 animate-pulse" />
+          <div className="w-20 h-20 bg-gray-200 animate-pulse" />
+          <div className="w-20 h-20 bg-gray-200 animate-pulse" />
+        </div>
+      }
+    >
+      <ArticleList />
+    </Suspense>
+  );
 }
